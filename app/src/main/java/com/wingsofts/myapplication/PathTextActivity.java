608a365a19c3ea83a47cfbd9abd24c663d0edb36
huangcong <huangcong@360.cn>
2018-09-08 10:11:22 +0800

    addcode

---
+++ app/src/main/java/com/wingsofts/myapplication/PathTextActivity.java
@@ -0,0 +1,16 @@
+package com.wingsofts.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wingsofts.threedlayout.ThreeDLayout;
+
+public class PathTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_path_text);
+
+    }
+}
